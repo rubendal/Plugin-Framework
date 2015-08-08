c0b93917f3803e3b0510052853ae0d6efe0479aa
rubendal <rubendal@users.noreply.github.com>
2015-08-08 01:41:57 -0500

    Initial

---
+++ Plugin/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Plugin
+{
+    public class Plugin : PluginClass.PluginClass
+    {
+
+        public override void Run()
+        {
+            //Do something...
+            Form1 f = new Form1();
+            f.ShowDialog();
+        }
+
+        
+    }
+}
